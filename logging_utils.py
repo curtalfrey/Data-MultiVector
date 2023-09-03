9c74aa9764bbe142df97651f44fbb0ccd1e91435
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-03 10:52:55 -0500

    Delete logging_utils.py

--- logging_utils.py
+++
@@ -1,8 +0,0 @@
-# logging_utils.py
-import logging
-
-def configure_logging():
-    # Configure logging settings
-
-def log_data_modification(connection, modified_data):
-    # Log data modification events
