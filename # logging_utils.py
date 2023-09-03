65440dfca61da3c5a5fa390e8f24005129e47834
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-03 10:26:49 -0500

    Create # logging_utils.py

---
+++ # logging_utils.py
@@ -0,0 +1,8 @@
+# logging_utils.py
+import logging
+
+def configure_logging():
+    # Configure logging settings
+
+def log_data_modification(connection, modified_data):
+    # Log data modification events
