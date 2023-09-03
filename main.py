19c738b01c7bc3426c82c34ea253b5dc58ae20f6
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-03 10:55:39 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from core import Connection, route_data
-from web_ui import app
-from logging_utils import configure_logging, log_data_modification
-from monitoring import ConnectionMonitor
-
-if __name__ == "__main__":
-    # Initialize connections, configure logging, set up monitoring, and start the app
