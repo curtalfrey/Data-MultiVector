e2105680e540682e5378238285058256ce490bfd
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-04 16:27:32 -0500

    Update app.py
    added
    html, css, error correcting, user options, core file handling logic, mysql, trash

--- app.py
+++ app.py
@@ -2,77 +2,70 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
+import shutil
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'  # Replace with your MySQL configuration
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# ...
 
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+# User model and other code remain unchanged
 
-# User model
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        
-        hashed_password = generate_password_hash(password, method='bcrypt')
-        
-        new_user = User(username=username, email=email, password_hash=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        
-        flash('Registration successful. Please log in.', 'success')
-        return redirect(url_for('login'))
-    
-    return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        
-        if user and check_password_hash(user.password_hash, password):
-            login_user(user)
-            flash('Login successful.', 'success')
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Invalid username or password.', 'danger')
-            return redirect(url_for('login'))  # Redirect to login page with flash message
-    
-    return render_template('login.html')
-
-@app.route('/dashboard')
+@app.route('/delete/<filename>')
 @login_required
-def dashboard():
-    return render_template('dashboard.html', user_data=current_user)
+def delete_file(filename):
+    upload_folder = app.config['UPLOAD_FOLDER']
+    user_trash_folder = os.path.join(app.root_path, 'trash', current_user.username)
 
-@app.route('/logout')
+    if not os.path.exists(user_trash_folder):
+        os.makedirs(user_trash_folder, exist_ok=True)
+    
+    file_path = os.path.join(upload_folder, filename)
+
+    if os.path.exists(file_path):
+        # Move file to user's trash folder
+        trash_path = os.path.join(user_trash_folder, filename)
+        os.rename(file_path, trash_path)
+        flash(f'File {filename} moved to trash.', 'success')
+    else:
+        flash(f'File {filename} not found.', 'danger')
+
+    return redirect(url_for('files'))
+
+@app.route('/trash')
 @login_required
-def logout():
-    logout_user()
-    flash('Logged out successfully.', 'success')
-    return redirect(url_for('index'))
+def trash():
+    user_trash_folder = os.path.join(app.root_path, 'trash', current_user.username)
+    trashed_files = []
+
+    for filename in os.listdir(user_trash_folder):
+        file_path = os.path.join(user_trash_folder, filename)
+        if os.path.isfile(file_path):
+            trashed_files.append({
+                'filename': filename,
+                'size': os.path.getsize(file_path),
+                'url': url_for('recovery', filename=filename),
+            })
+
+    return render_template('trash.html', trashed_files=trashed_files)
+
+@app.route('/recover/<filename>')
+@login_required
+def recovery(filename):
+    user_trash_folder = os.path.join(app.root_path, 'trash', current_user.username)
+    trash_path = os.path.join(user_trash_folder, filename)
+    upload_folder = app.config['UPLOAD_FOLDER']
+
+    if os.path.exists(trash_path):
+        # Move file back from trash to the original folder
+        restored_path = os.path.join(upload_folder, filename)
+        shutil.move(trash_path, restored_path)
+        flash(f'File {filename} has been restored.', 'success')
+    else:
+        flash(f'File {filename} not found in trash.', 'danger')
+
+    return redirect(url_for('trash'))
+
+# ...
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
