4a6aa8e9e6063d1f95d38ba95595558802bb2a09
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-04 14:18:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import threading
 import logging
-import datetime  # Import the datetime module
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -20,49 +20,46 @@ change_logs = {}
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        input_paths = request.form.getlist('input_paths[]')
-        output_paths = request.form.getlist('output_paths[]')
-        connections = request.form.getlist('connections[]')
+        try:
+            input_paths = request.form.getlist('input_paths[]')
+            output_paths = request.form.getlist('output_paths[]')
+            connections = request.form.getlist('connections[]')
 
-        # Update processing and storage logic
-        for idx, connection in enumerate(connections):
-            connection_id = f"connection_{idx}"
-            connection_data[connection_id] = {
-                'input_path': input_paths[idx],
-                'output_path': output_paths[idx],
-                'processing_status': 'pending',  # Add processing status attribute
-                'timestamp': datetime.datetime.now(),  # Add timestamp attribute
-                # Add more relevant data fields as needed
-            }
-            
-            change_logs[connection_id] = []
-            
-            # Log initial change
-            change_logs[connection_id].append(f"Initial setup: {input_paths[idx]} -> {output_paths[idx]}")
-            
-            # Implement data processing logic here
-            apply_transformations(connection_id)
-            
-            # Demonstrate processing with print messages
-            print(f"Processing connection {connection_id}: {input_paths[idx]} -> {output_paths[idx]}")
-            
-            # Log applied transformations in the change log
-            change_logs[connection_id].append("Applied transformation A")
-            change_logs[connection_id].append("Applied transformation B")
-        
-        # For demonstration purposes, print processed data and change logs
-        print("Connection Data:", connection_data)
-        print("Change Logs:", change_logs)
+            # Update processing and storage logic
+            for idx, connection in enumerate(connections):
+                connection_id = f"connection_{idx}"
+                connection_data[connection_id] = {
+                    'input_path': input_paths[idx],
+                    'output_path': output_paths[idx],
+                    'processing_status': 'pending',
+                    'timestamp': datetime.datetime.now(),
+                }
+
+                change_logs[connection_id] = []
+
+                # Log initial change
+                change_logs[connection_id].append(f"Initial setup: {input_paths[idx]} -> {output_paths[idx]}")
+
+                # Implement data processing logic here
+                apply_transformations(connection_id)
+
+                # Demonstrate processing with print messages
+                print(f"Processing connection {connection_id}: {input_paths[idx]} -> {output_paths[idx]}")
+
+                # Log applied transformations in the change log
+                change_logs[connection_id].append("Applied transformation A")
+                change_logs[connection_id].append("Applied transformation B")
+
+            # For demonstration purposes, print processed data and change logs
+            print("Connection Data:", connection_data)
+            print("Change Logs:", change_logs)
+
+        except Exception as e:
+            error_message = f"An error occurred: {str(e)}"
+            print(error_message)
+            return render_template('error.html', error_message=error_message)
 
     return render_template('index.html')
-
-@app.route('/developer')
-def developer():
-    return render_template('developer.html')
-
-@app.route('/documentation')
-def documentation():
-    return render_template('documentation.html')
 
 # ... (other routes remain unchanged)
 
