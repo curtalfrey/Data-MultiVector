6474f06182255231ed5e9dbcdd25db134dd4ec24
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-04 13:07:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    totp_code = StringField('TOTP Code', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -36,6 +42,26 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        totp_code = form.totp_code.data
+
+        if username in users_db and hashlib.sha256(password.encode()).hexdigest() == users_db[username]['password']:
+            totp = pyotp.TOTP(users_db[username]['totp_secret'])
+            if totp.verify(totp_code):
+                flash('Login successful!', 'success')
+            else:
+                flash('Invalid TOTP code.', 'error')
+        else:
+            flash('Invalid username or password.', 'error')
+
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
