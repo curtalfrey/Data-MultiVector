64ffe2e74fbd501bb221a4c499d51173074a400c
Curt Alfrey <79066679+curtalfrey@users.noreply.github.com>
2023-09-04 11:37:46 -0500

    Update data_processing_script.py

--- data_processing_script.py
+++ data_processing_script.py
@@ -1,78 +1,22 @@
-# data_processing_script.py
+# ... (other imports and code remain the same) ...
 
-# This script defines a Flask application for data processing.
-# It includes instructions, variables, routes, and documentation.
-
-import os
-import threading
-import logging
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# Enable Flask debugger mode
-app.debug = True
-
-# Logging configuration
-logging.basicConfig(filename='script.log', level=logging.INFO)
-
-# Dictionary to store data for each connection
-connection_data = {}
-
-# Dictionary to store change logs for each connection
-change_logs = {}
-
-# Instructions for the user
-INSTRUCTIONS = """
-<b>Instructions:</b><br>
-1. Log in using your credentials.<br>
-2. Add input and output paths to create connections.<br>
-3. Connections will be auto-generated with unique names.<br>
-4. The program acts as a symlink, showing data from input paths in output paths.<br>
-5. Monitor and track changes in the output files.<br>
-"""
-
-# Additional User Variables
-# Add any extra variables you need from the user in this section.
-# For example, you can request the user to specify file paths or directories.
-USER_VARIABLES = {
-    "input_paths": "",
-    "output_paths": "",
-}
-
-# Documentation
-DOCUMENTATION_TEXT = """
-<h1>Documentation</h1>
-
-<p>Welcome to Data-MultiVector! This program allows you to create connections between input and output paths, acting as symlinks to display the same data in the output files as the input files.</p>
-
-<h2>Website:</h2>
-<p>Visit our website for more information and updates: <a href="http://data-multivector.curtisalfrey.com">http://data-multivector.curtisalfrey.com</a></p>
-
-<h2>FAQ:</h2>
-<p>Check our Frequently Asked Questions for common queries and solutions.</p>
-
-<h2>Troubleshooting Guide:</h2>
-<p>If you encounter any issues, consult our troubleshooting guide for assistance.</p>
-
-<h2>Install Instructions:</h2>
-<p>Follow the installation instructions to set up Data-MultiVector on your system.</p>
-
-<p>We hope you find Data-MultiVector useful for managing your data connections!</p>
-"""
-
-# ... (rest of the script remains unchanged) ...
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', instructions=INSTRUCTIONS, user_variables=USER_VARIABLES)
+    if request.method == 'POST':
+        input_paths = request.form.getlist('input_paths[]')
+        output_paths = request.form.getlist('output_paths[]')
+        connections = request.form.getlist('connections[]')
 
-@app.route('/documentation')
-def documentation():
-    return render_template('documentation.html', documentation_text=DOCUMENTATION_TEXT)
+        # Process the input_paths, output_paths, and connections as needed
+        # For demonstration purposes, let's just print them
+        print("Input Paths:", input_paths)
+        print("Output Paths:", output_paths)
+        print("Selected Connections:", connections)
 
-# ... (other routes and functions remain unchanged) ...
+    return render_template('index.html', instructions=INSTRUCTIONS)
+
+# ... (other routes and functions remain the same) ...
 
 if __name__ == "__main__":
-    # Run Flask app with debugger enabled
     app.run(host="0.0.0.0", port=5000, debug=True)
+
